4399be96350f3a2d6aa01a44e24b95c8b7ece5e2
taiawu <taiawu@gmail.com>
2021-05-09 01:23:15 -0700

    updated plateview_plot() to handle variable well column names, add empty wells,  and add "row" and "column" columns.

--- R/plateview_plot.R
+++ R/plateview_plot.R
@@ -4,6 +4,7 @@
 #'
 #' @param layout_data a layout tibble (as created by dsfworld::read_plate_layout()), containing columns for plate row (called "row"), plate column (called "column"), and one variable by which the plate will be colored
 #' @param .fill_var the column in the input layout by which to color in the wells in the plot
+#' @param .well_col the name of the column containing the wells. Defaults to "well".
 #' @param col_breaks the number of tick marks and lines in the x axis (columns). Defaults to every 6 columns.
 #' @param .title title for the plot. Defaults to "Plate-view plot"
 #' @param .scale_fill the color scale to use in the plot. Defaults to scale_color_viridis_d(). Must be updated when plotting continuous variables (e.g. to scale_color_viridis_c())
@@ -11,7 +12,7 @@
 #' @return a plate-view plot, with wells colored based on the information on that well in the user-defined variable given in .fill_var.
 #'
 #' @importFrom magrittr "%>%"
-#' @importFrom dplyr filter
+#' @importFrom dplyr filter rename
 #' @importFrom ggplot2 ggplot aes scale_y_discrete geom_point scale_x_continuous theme labs scale_fill_viridis_d element_rect element_blank element_line
 #' @importFrom plyr rbind.fill
 #' @importFrom tidyselect any_of
@@ -24,9 +25,14 @@
 #'
 plateview_plot <- function(layout_data,
                        .fill_var,
+                       .well_col = "well",
                        col_breaks = 6,
                        .title = "Plate-view plot",
                        .scale_fill = scale_fill_viridis_d()) {
+
+  layout_data <- layout_data %>%
+                      rename(well = .well_col) %>%
+                        add_empty_wells( . )
 
   plate_data <- layout_data %>%
     filter(is.na( {{ .fill_var }}) == FALSE)
